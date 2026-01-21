517451e2b24a59990c0f3d0786e552a290c3aea8
devdemon713 <kolheprashant456@gmail.com>
2026-01-21 19:43:22 +0530

    third comit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://ai-chatbox-backend.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default api;
-
-
